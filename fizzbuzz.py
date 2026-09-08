21818e82e747a7179bf45865c9c9b7cc097be542
tyttt <theyt1025@gmail.com>
2026-09-08 22:23:33 +0800

    add all six algorithms

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    for i in range (1, 21):
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizzbuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
